6c7d6c2e7fa9229b6f2d40405f3d117fc5b74396
shibumav17 <shibuchandel17@gmail.com>
2022-04-08 13:07:12 +0530

    variable manipulation

---
+++ src/variable/Add_Sub_Multi_Divide.java
@@ -0,0 +1,46 @@
+package variable;
+
+public class Add_Sub_Multi_Divide {
+    public static void main(String[] args) {
+        // Data_type var_name = Value:
+
+        int a = 5;
+        int b = 7;
+
+        int sum = a+b;
+        System.out.println("Sum of a & b is " + sum);
+        System.out.println("Advance Sum of a & b is " + (a+b));
+
+
+        int sub = a-b;
+        System.out.println("Subtraction of a & b is " + sub);
+        System.out.println("Advance subtraction of a & b is " + (a-b));
+
+
+        a = 60;
+        b = 10;
+        int divide = a/b;
+        System.out.println("division of a & b is " +divide);
+        System.out.println("Advance division of a & b is " + a/b);
+
+
+        int multi = a*b;
+        System.out.println("multiplication of a & b is " +multi);
+        System.out.println("Advance multiplication of a & b is " + a*b);
+
+
+        double c = 10.5;
+        double d = 1.5;
+        double add = c+d;
+        System.out.println("Additon of c & d is " +add);
+        System.out.println("Advance Additon of c & d is " +(c+d));
+
+
+
+        a = 9 ;
+        b = 3;
+        System.out.println("Division of a & b is " + a/b);
+        System.out.println("Advance division of c & d is " +a/b);
+
+    }
+}
